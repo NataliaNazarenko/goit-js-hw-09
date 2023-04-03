faf1d6b31c7741b1f8fa4735a274f3c9c21b4869
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-04-03 20:46:03 +0000

    Deploying to gh-pages from @ NataliaNazarenko/goit-js-hw-09@34fd889e9253e1467922c1ed049412c4e281e61f 🚀

--- 01-color-switcher.0769dade.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){n=setInterval((function(){t.startBtn&&(document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.startBtn.disabled=!0,t.stopBtn.disabled=!1)}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.0769dade.js.map
