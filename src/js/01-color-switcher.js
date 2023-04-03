ec3fea66591880a93db6bf16603d965d986c70fc
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-04-03 23:29:54 +0300

    add solving 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+refs.stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    if (refs.startBtn) {
+      document.body.style.backgroundColor = getRandomHexColor();
+      refs.startBtn.disabled = true;
+      refs.stopBtn.disabled = false;
+    }
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+});
